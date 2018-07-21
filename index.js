3ed63f9fcfc5f76fbb1699ba18e35fb714299db4
Arvind Thangamani <dnivra26@gmail.com>
2018-07-21 12:45:00 +0530

    divide circles into equal parts

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function draw_divide_circle() {
+    var c=document.getElementById("canvas");
+    var ctx=c.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    circle_x = 350;
+    circle_y = 350;
+    circle_radius = document.getElementById("radius").value;
+    ctx.arc(circle_x,circle_y,circle_radius,0,2*Math.PI);
+    ctx.stroke();
+    var number_of_parts = document.getElementById("no_segments").value;
+    for(i=0; i < number_of_parts; i++) {
+        var angle_i = (2*i*Math.PI)/number_of_parts;
+        var x_i = (circle_radius*Math.cos(angle_i)) + circle_x;
+        var y_i = (circle_radius*Math.sin(angle_i)) + circle_y;
+        ctx.beginPath();
+        ctx.moveTo(circle_x,circle_y);
+        ctx.lineTo(x_i,y_i);
+        ctx.stroke();
+    }
+}
+
+function download_canvas() {
+    document.getElementById("downloader").download = "image.png";
+    document.getElementById("downloader").href = document.getElementById("canvas").toDataURL("image/png").replace(/^data:image\/[^;]/, 'data:application/octet-stream');
+}
