97978e723e1840d7c9cc9452cad56d7083a8202e
Arvind Thangamani <dnivra26@gmail.com>
2018-07-22 10:10:01 +0530

    add segment type as user selectable

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ function draw_divide_circle() {
     ctx.arc(circle_x,circle_y,circle_radius,0,2*Math.PI);
     ctx.stroke();
     var number_of_parts = document.getElementById("no_segments").value;
+    var segment_type = document.querySelector('input[name="segment_type"]:checked').value;
     for(i=0; i < number_of_parts; i++) {
         var angle_i = (2*i*Math.PI)/number_of_parts;
         var x_i = (circle_radius*Math.cos(angle_i)) + circle_x;
         var y_i = (circle_radius*Math.sin(angle_i)) + circle_y;
-        ctx.beginPath();
-        ctx.moveTo(circle_x,circle_y);
-        ctx.lineTo(x_i,y_i);
-        ctx.stroke();
+        if (segment_type == 'line') {
+            ctx.beginPath();
+            ctx.moveTo(circle_x,circle_y);
+            ctx.lineTo(x_i,y_i);
+            ctx.stroke();
+        } else {
+            ctx.fillRect(x_i,y_i,3,3);
+        }
     }
 }
 
